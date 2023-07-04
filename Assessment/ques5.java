1a0e7f28c47986cf9384c3e4638b0876793c4bd7
falguni0607 <91118043+falguni0607@users.noreply.github.com>
2023-07-04 23:18:17 +0530

    Add files via upload

---
+++ Assessment/ques5.java
@@ -0,0 +1,13 @@
+public class ques5 {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+        String name = "John Doe";
+
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+    
+}
